95638d8783d5f5edb60f926b1858f08ee4ffa97c
= <=>
2015-05-26 19:51:07 -0500

    Inital commit because reasons.

---
+++ src/main/java/com/wiggle1000/HxC/Diseases/HxCDiseases.java
@@ -0,0 +1,48 @@
+package com.wiggle1000.HxC.Diseases;
+
+import com.wiggle1000.HxC.Diseases.items.*;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionHelper;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+@Mod(modid = HxCDiseases.MODID, version = HxCDiseases.VERSION, dependencies = "required-after:HxCCore")
+public class HxCDiseases
+{
+	
+    public static final String MODID = "hxcdiseases";
+    public static final String VERSION = "1.0";
+    
+    public ItemVial SwineFlu;
+    
+    public static CreativeTabs tabDiseases = new CreativeTabs("tabDiseases") {
+	    @Override
+	    @SideOnly(Side.CLIENT)
+	    public Item getTabIconItem() {
+	        return Items.poisonous_potato;
+	    }
+	};
+	
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	
+    	//DECLARE--------------------------------
+    	//FLU
+    	SwineFlu = new ItemVial("Swine Flu",new Potion[]{Potion.digSlowdown,Potion.confusion,Potion.poison,Potion.weakness,Potion.hunger,Potion.moveSlowdown}, 10000);
+    	
+    	
+    	//REGISTER-------------------------------
+    	GameRegistry.registerItem(SwineFlu, "vialswineflu");
+    }
+}
---
+++ src/main/java/com/wiggle1000/HxC/Diseases/items/ItemVial.java
@@ -0,0 +1,84 @@
+package com.wiggle1000.HxC.Diseases.items;
+
+import java.util.List;
+
+import com.wiggle1000.HxC.Diseases.HxCDiseases;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionAttackDamage;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.potion.PotionHelper;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.world.World;
+
+public class ItemVial extends ItemFood{
+	
+
+	public String diseasename = "Default Disease";
+	public Potion[] effects;
+	public int duration = 120;
+	
+	public ItemVial(String _diseasename, Potion[] _effects, int _duration){
+		super(0,false);
+		this.diseasename = _diseasename;
+		this.effects = _effects;
+		this.duration = _duration;
+		this.setCreativeTab(HxCDiseases.tabDiseases);
+		this.setHasSubtypes(true);
+		this.setAlwaysEdible();
+		this.setUnlocalizedName("vial_"+diseasename.replace(" ", "").toLowerCase());
+		this.setTextureName(diseasename.replace(" ", "").toLowerCase()+"_vial");
+	}
+
+	@Override
+	public EnumAction getItemUseAction(ItemStack item){
+		return EnumAction.eat;
+	}
+	
+	@Override
+	public ItemStack onEaten(ItemStack item, World world, EntityPlayer player){
+		applyDisease(player);
+		return new ItemStack(item.getItem(),item.stackSize-1);
+	}
+	
+	@Override
+	public boolean onLeftClickEntity(ItemStack is, EntityPlayer myPlayer, Entity other){
+		applyDisease(other);
+		return true;
+	}
+	//keldon is an ass
+	public void applyDisease(Entity player){
+		if(!player.worldObj.isRemote){
+			if(player instanceof EntityPlayer){
+				((EntityPlayer)player).addChatMessage(new ChatComponentText("You now have '"+this.diseasename+"'!"));
+			}
+			if(player instanceof EntityLiving){
+				for(Potion p: effects){
+					((EntityLiving)player).addPotionEffect(new PotionEffect(p.id, duration, 1));
+				}
+			}else if(player instanceof EntityPlayer){
+				for(Potion p: effects){
+					((EntityPlayer)player).addPotionEffect(new PotionEffect(p.id, duration, 1));
+				}
+			}
+		}
+	}
+	@Override
+    @SideOnly(Side.CLIENT)
+    public void getSubItems(Item item, CreativeTabs creativeTabs, List list){
+        for(int i = 0; i < 9; i++){
+            list.add(new ItemStack(item, 1, i));
+        }
+    }
+}
