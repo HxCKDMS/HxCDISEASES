f5fd1ba045faffd51d1549e3caf2ebacf1ad2b9c
wiggle1000 <austintempletonthepawner@yahoo.com>
2016-11-28 19:53:27 -0600

    Slight updates towards server compat for incubators..

--- src/main/java/HxCKDMS/HxCDiseases/blocks/TileEntityIncubator.java
+++ src/main/java/HxCKDMS/HxCDiseases/blocks/TileEntityIncubator.java
@@ -6,6 +6,9 @@ import HxCKDMS.HxCDiseases.Utilities;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.NetworkManager;
+import net.minecraft.network.Packet;
+import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 
@@ -21,12 +24,14 @@ public class TileEntityIncubator extends TileEntity
     public float progress = 0;
     public IncubatorRecipe making;
     private Random rand = new Random();
+
     @Override
     public void updateEntity() {
         mixing = processing;
         worldObj.getEntitiesWithinAABB(EntityItem.class, AxisAlignedBB.getBoundingBox(xCoord,yCoord+1,zCoord,xCoord+1,yCoord+2,zCoord+1)).forEach((Object oItem)->{
             EntityItem item = (EntityItem)oItem;
             NBTTagCompound nbt = item.getEntityItem().getTagCompound();
+            markDirty();
             if(!processing) {
                 HxCDiseases.incubatorRecipes.forEach((IncubatorRecipe recipe) -> {
                     if (Utilities.isSameDiseaseItem(recipe.input, item.getEntityItem())) {
@@ -57,6 +62,7 @@ public class TileEntityIncubator extends TileEntity
                 progressCounter++;
                 if(progressCounter%60==0 && progressCounter < target-60){
                     worldObj.playSoundEffect(xCoord,yCoord,zCoord,"hxcdiseases:incuwork",1.5f,1);
+                    markDirty();
                 }
             }else{
                 if(!worldObj.isRemote) {
@@ -73,13 +79,56 @@ public class TileEntityIncubator extends TileEntity
     }
 
     private void outputItem(){
-    status = "Culture grown successfully!";
+        status = "Culture grown successfully!";
         EntityItem item = new EntityItem(worldObj, xCoord, yCoord + 2, zCoord, making.output.copy());
         worldObj.spawnEntityInWorld(item);
+        markDirty();
+    }
+
+    @Override
+    public void readFromNBT(NBTTagCompound tag) {
+
+        super.readFromNBT(tag);
+        status = tag.getString("status");
+        mixing = tag.getBoolean("mixing");
+        processing = tag.getBoolean("processing");
+        progressCounter = tag.getInteger("progressCounter");
+        target = tag.getInteger("target");
+        if(tag.hasKey("recipeIndex")) {
+            making = HxCDiseases.incubatorRecipes.get(tag.getInteger("recipeIndex"));
+        }
+
+    }
+
+    @Override
+    public void writeToNBT(NBTTagCompound tag) {
+
+        super.writeToNBT(tag);
+        tag.setString("status", status);
+        tag.setBoolean("mixing", mixing);
+        tag.setBoolean("processing", processing);
+        tag.setInteger("progressCounter", progressCounter);
+        tag.setInteger("target", target);
+        if(making!=null) {
+            tag.setInteger("recipeIndex", HxCDiseases.incubatorRecipes.indexOf(making));
+        }
+    }
+
+    @Override
+    public Packet getDescriptionPacket() {
+        NBTTagCompound tag = new NBTTagCompound();
+        this.writeToNBT(tag);
+        return new S35PacketUpdateTileEntity(xCoord, yCoord, zCoord, 1, tag);
+    }
+
+    @Override
+    public void onDataPacket(NetworkManager net, S35PacketUpdateTileEntity packet) {
+        readFromNBT(packet.func_148857_g());
     }
 
     private void fail(){
         worldObj.playSoundEffect(xCoord,yCoord,zCoord,"hxcdiseases:fail",1,1);
         status = "Failed to grow culture.";
+        markDirty();
     }
 }
